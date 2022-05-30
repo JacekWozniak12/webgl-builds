faec54c2322b9461a9969258f4db2d397d25566c
Jacek Woźniak <jac3kw0zniak@gmail.com>
2022-05-31 00:32:52 +0200

    extenject-unity-api app

---
+++ extenject-unity-api/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Jacek Woźniak-extenject-unity-api-1";
+const contentToCache = [
+    "Build/extenject-unity-api.loader.js",
+    "Build/extenject-unity-api.framework.js.unityweb",
+    "Build/extenject-unity-api.data.unityweb",
+    "Build/extenject-unity-api.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
